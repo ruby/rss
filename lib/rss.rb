3dea1eff69b128d693e1b41b29f5bbb74464752f
Sutou Kouhei <kou@clear-code.com>
2020-04-17 06:48:14 +0900

    Use double quote for string literal

--- lib/rss.rb
+++ lib/rss.rb
@@ -79,16 +79,16 @@ end
 
 require "rss/version"
 
-require 'rss/1.0'
-require 'rss/2.0'
-require 'rss/atom'
-require 'rss/content'
-require 'rss/dublincore'
-require 'rss/image'
-require 'rss/itunes'
-require 'rss/slash'
-require 'rss/syndication'
-require 'rss/taxonomy'
-require 'rss/trackback'
+require "rss/1.0"
+require "rss/2.0"
+require "rss/atom"
+require "rss/content"
+require "rss/dublincore"
+require "rss/image"
+require "rss/itunes"
+require "rss/slash"
+require "rss/syndication"
+require "rss/taxonomy"
+require "rss/trackback"
 
 require "rss/maker"
