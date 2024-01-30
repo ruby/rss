ca6bf2b75d5b83b658f20c2f85538209ef601496
Timofey <44918629+timofey282228@users.noreply.github.com>
2024-01-30 03:07:19 +0200

    doc: use URI.open in rss.rb rdoc
    Fix example usage of deprecated Kernel#open override
    https://github.com/ruby/open-uri/commit/53862fa35887a34a8060aebf2241874240c2986a

--- lib/rss.rb
+++ lib/rss.rb
@@ -26,7 +26,7 @@
 #   require 'open-uri'
 #
 #   url = 'http://www.ruby-lang.org/en/feeds/news.rss'
-#   open(url) do |rss|
+#   URI.open(url) do |rss|
 #     feed = RSS::Parser.parse(rss)
 #     puts "Title: #{feed.channel.title}"
 #     feed.items.each do |item|
